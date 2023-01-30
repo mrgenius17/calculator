3c9424788717156ed3b01bbccdeda2c5199fc2c9
mrgenius17 <darshanthakkar716@gmail.com>
2023-01-30 15:54:30 +0530

    Add functionality to buttons

--- script.js
+++ script.js
@@ -28,3 +28,37 @@ function operate(operator,a,b) {
         return divide(a,b);
     }
 }
+var operand1 = null;
+var operand2 = null;
+var operator = null;
+
+const display = document.querySelector('#display-container');
+
+const digits = document.querySelector("#digits-div").querySelectorAll('button');
+digits.forEach(button => button.addEventListener('click', () => {
+    display.textContent += button.id; 
+}));
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach(operatorButton => operatorButton.addEventListener('click', () => {
+    operator = operatorButton.id;
+    operand1 = +(display.textContent);
+    display.textContent = "";
+}));
+
+const equalButton = document.querySelector('#equal');
+equalButton.addEventListener('click', () => {
+    operand2 = +(display.textContent);
+    if(!(operand1 || operand2)){
+        console.log(null);
+        return;
+    }
+
+    display.textContent = operate(operator, operand1, operand2);
+})
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', () => {
+    display.textContent = "";
+    operand1 = operand2 = operator = null;
+})
